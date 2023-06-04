1f874a0346b51ada2b6799294506fd803ef4a648
TSABET2004 <127050347+TSABET2004@users.noreply.github.com>
2023-06-04 20:14:10 +0100

    Add files via upload

--- lib/presentaion/pages/home_page.dart
+++ lib/presentaion/pages/home_page.dart
@@ -9,110 +9,137 @@ class HomePage extends StatefulWidget {
 
 
 class _HomePageState extends State<HomePage> {
-  String player1Name = "Abbache";
-  String player2Name = "Mestafa";
+  String player1Name = "Mayar";
+  String player2Name = "Nada";
   int player1Score = 0;
   int player2Score = 0;
-
   bool player1Start = true;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[100],
-      body: Container( 
-       height: 1,
-       width: double.infinity,
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.start,
-         crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.amberAccent[200],
-                padding: const EdgeInsets.only(top: 0),
-                child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.white,
-                        alignment: Alignment.center,
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          "X",
-                          style: const TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        player1Name,
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.white,
-                        alignment: Alignment.center,
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          "O",
-                          style: const TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        player2Name,
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  )
-                ]),
-              ),
-          Container(
-                height: 400,
-                width: double.infinity,
-                color: Colors.greenAccent,
-              ),
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: Colors.white,
-            padding: const EdgeInsets.all(10),
-            child: Row(
+      body: SingleChildScrollView(
+        child: Container( 
+         width: double.infinity,
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                   child: Icon(Icons.replay),
+                  height: 200,
+                  width: double.infinity,
+                  color: Colors.white,
+                  padding: const EdgeInsets.only(top: 0),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Color.fromARGB(255, 214, 138, 227),
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            "X",
+                            style: const TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Mayar',
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: 50.0,
-                    ),
-                    Container(
-                      child: Text('Start'),
-                    ),
-                   ],
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Color.fromARGB(255, 214, 138, 227),
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            "O",
+                            style: const TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Nada',
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
+                ),
+            Container(
+                  height: 400,
+                  width: double.infinity,
+                  color: Color.fromARGB(255, 214, 138, 227),
+                  child:  GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 3,
+              ),
+              children: [
+                for(int i = 0;i < 9;i++)
+                Container(color: Color.fromARGB(255, 230, 202, 235)),
+              ],
+          ),
+                ),
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.white,
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Container(
+                     child: ElevatedButton.icon(
+                      onPressed:  () {},
+                       icon: Icon(Icons.replay,
+                       size: 30.0,
+                       color: Color.fromARGB(255, 203, 70, 25),
+                       ),
+                        label: Text('Play again'),
+                     ),
                   ),
-                ),    
-        ],
-       ), 
+                  SizedBox(
+                        width: 50.0,
+                  ),
+                  Container(
+                    child: ElevatedButton.icon(
+                     onPressed:  () {},
+                     icon: Icon(Icons.start,
+                     size: 30.0,
+                     color: Color.fromARGB(255, 203, 70, 25),
+                     ),
+                     label: Text('Start'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+         ), 
+        ),
       ),
     );
   }
